601c611f5bae55539f3db2fb1f6676d7dd9cbea4
asb1512 <andrew@olympicmedia.com>
2021-08-19 22:36:18 -0400

    created redux store and wrapped App component with Redux provider

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk'
 import {createStore, applyMiddleware} from 'redux';
+import parksReducer from './reducers/parksReducer';
+
+const store = createStore(parksReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
