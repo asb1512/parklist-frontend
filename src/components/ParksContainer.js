4153b761455b3b817c55f46b9ad0387199aac480
asb1512 <andrew@olympicmedia.com>
2021-08-21 02:02:54 -0400

    rendering ParksList with state passed as props

--- src/components/ParksContainer.js
+++ src/components/ParksContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ParksList from './ParksList';
 
 class ParksContainer extends Component {
 
   render() {
     return (
-      <div>ParksContainer</div>
+      <div>
+        <ParksList parks={this.props.parks} loading={this.props.loading} />
+      </div>
     )
   }
 }
