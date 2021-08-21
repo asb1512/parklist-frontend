be51c595f738ee5ec5ce26f673eb2ca14ee7e438
asb1512 <andrew@olympicmedia.com>
2021-08-20 23:05:04 -0400

    removed React Strict Mode

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
 import parksReducer from './reducers/parksReducer';
 
-const composedEnhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
+const composedEnhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 const store = createStore(parksReducer, undefined, composedEnhancer);
+// const store = createStore(parksReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
