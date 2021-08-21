2d7f298ec490939fd322e5e893bdce224444877e
asb1512 <andrew@olympicmedia.com>
2021-08-20 23:06:17 -0400

    removed current "LOADING_PARKS" body of action for debugging

--- src/reducers/parksReducer.js
+++ src/reducers/parksReducer.js
@@ -1,6 +1,21 @@
-export default function parksReducer (state = { name: '', location: '', image: '', loading: false }, action) {
+export default function parksReducer(state = [], action) {
 
   switch (action.type) {
+
+    case "LOADING_PARKS":
+      // return {
+      //   ...state,
+      //   parks: [...state.parks],
+      //   loading: true
+      // }
+      return {state}
+
+    case "ADD_PARKS":
+      return {
+        ...state,
+        parks: action.parks,
+        loading: false
+      }
 
     case "ADD_PARK_TO_USER":
       return { state }
