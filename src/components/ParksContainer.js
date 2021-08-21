6bf0033ec7cb6c56ee4af8bf07918300f22783bf
asb1512 <andrew@olympicmedia.com>
2021-08-21 02:29:36 -0400

    added conatiner, row, and column components

--- src/components/ParksContainer.js
+++ src/components/ParksContainer.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import ParksList from './ParksList';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 class ParksContainer extends Component {
 
   render() {
     return (
-      <div>
-        <ParksList parks={this.props.parks} loading={this.props.loading} />
-      </div>
+      <Container>
+        <Row>
+          <Col>
+          {this.props.loading ?
+            "Loading" : <ParksList parks={this.props.parks} loading={this.props.loading} />
+          }
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
