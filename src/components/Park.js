7b24ae44fdac237e0b75debcc235db09acdb88fd
asb1512 <andrew@olympicmedia.com>
2021-08-22 20:14:03 -0400

    created Park component to display when a park's card is clicked

---
+++ src/components/Park.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Image from 'react-bootstrap/Image';
+
+class Park extends Component {
+
+  render() {
+    return (
+      <div>Park Component here!</div>
+    )
+  }
+}
+
+export default Park;
