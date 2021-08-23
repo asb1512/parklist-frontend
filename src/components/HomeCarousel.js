fa678d698b6648298645f2ea8d3e4f6b1c672fa7
asb1512 <andrew@olympicmedia.com>
2021-08-23 19:10:03 -0400

    added React-Bootstrap carousel component

--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container'
+import Carousel from 'react-bootstrap/Carousel'
 
 class HomeCarousel extends Component {
 
   render() {
+    console.log("HomeCarousel render()", this.props.parks)
+    const parksArray = this.props.parks?.map(park => {
+      return (
+        <Carousel.Item interval={2000} key={park.name}>
+          <img
+            className="d-block w-100"
+            src={park.image}
+            alt={park.name}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      )
+    })
+
     return (
-      <div>Home Carousel Component</div>
+      <Carousel>
+        {parksArray}
+      </Carousel>
     )
   }
 }
