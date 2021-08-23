99277e072d4d7624b226e83ad5e49d3535c5ad21
asb1512 <andrew@olympicmedia.com>
2021-08-23 18:33:46 -0400

    React boilerplate for component

---
+++ src/components/HomeCarousel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class HomeCarousel extends Component {
+
+  render() {
+    return (
+      <div>Home Carousel Component</div>
+    )
+  }
+}
+
+export default HomeCarousel;
