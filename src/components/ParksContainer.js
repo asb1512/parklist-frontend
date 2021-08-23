59e3d2a678e37ab2eb32e6f6dc1bdef67b3f23f0
asb1512 <andrew@olympicmedia.com>
2021-08-23 00:13:56 -0400

    added supplementary render method to use when API is offline

--- src/components/ParksContainer.js
+++ src/components/ParksContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ParksList from './ParksList';
+// import apiResponse from './apiResponse';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -9,21 +10,40 @@ class ParksContainer extends Component {
 
   render() {
     console.log("Parks Container Render", this.props.parks)
+    // this return statement to be used when Rails API is actually working
     return (
       <Container>
         <Row>
+          {this.props.loading ? <div>Loading...</div> : null}
           <Col>
-            <ParksList parks={this.props.parks.slice(0,21)} />
+            <ParksList parks={this.props.parks?.slice(0,21)} />
           </Col>
           <Col>
-            <ParksList parks={this.props.parks.slice(21,42)} />
+            <ParksList parks={this.props.parks?.slice(21,42)} />
           </Col>
           <Col>
-            <ParksList parks={this.props.parks.slice(42)} />
+            <ParksList parks={this.props.parks?.slice(42)} />
           </Col>
         </Row>
       </Container>
     )
+
+    // use this return statement when the Rails API isn't working properly
+    // return (
+    //   <Container>
+    //     <Row>
+    //       <Col>
+    //         <ParksList parks={apiResponse.slice(0,21)} />
+    //       </Col>
+    //       <Col>
+    //         <ParksList parks={apiResponse.slice(21,42)} />
+    //       </Col>
+    //       <Col>
+    //         <ParksList parks={apiResponse.slice(42)} />
+    //       </Col>
+    //     </Row>
+    //   </Container>
+    // )
   }
 }
 
