173f031e71b083b3f3507881713c62860a337c7f
asb1512 <andrew@olympicmedia.com>
2021-08-23 16:48:31 -0400

    adding React-Bootstrap code to implement a modal to view individual parks

--- src/components/Park.js
+++ src/components/Park.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
 import Image from 'react-bootstrap/Image';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { connect } from 'react-redux';
 
 class Park extends Component {
+  state = {
+    show: true,
+  }
+
+  handleShow = () => {
+    console.log("Park.js/handleShow()")
+    this.setState({show: true})
+  }
+
+  handleClose = () => {
+    console.log("Park.js/handleClose()")
+    this.setState({show: false})
+  }
 
   render() {
+    console.log("Park render()", this.state.show)
     return (
-      <div>This is a Park component.</div>
+      <Modal show={this.state.show} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={this.handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     )
   }
 }
 
-export default Park;
+const mapStateToProps = state => {
+  return {}
+}
+
+export default connect(mapStateToProps, null)(Park);
