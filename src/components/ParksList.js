9903c3270b243160bc5cec6343400ffe1f115dc6
asb1512 <andrew@olympicmedia.com>
2021-08-25 08:25:02 -0400

    added func that will format park name as a path

--- src/components/ParksList.js
+++ src/components/ParksList.js
@@ -2,32 +2,37 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useRouteMatch } from 'react-router-dom';
 
 
-class ParksList extends Component {
+function ParksList(props) {
+  let match = useRouteMatch();
 
-  render() {
-    const parksArray = this.props.parks?.map(park => {
-      return (
-        <Card style={{ width: '18rem' }} key={park.name} className={"m3"} >
-          <Card.Img variant="top" src={park.small_image} alt={park.name} />
-          <Card.Body>
-            <Card.Title>{park.name}</Card.Title>
-            <Card.Text>
-              Located in {park.location}
-            </Card.Text>
-              <LinkContainer to={`${match.url}/${park.name.toLowerCase()}`}>
-                <Button
-                  // onClick={() => this.props.showPark(park)}
-                  variant="primary"
-                  className="rounded-pill"
-                >Discover
-                </Button>
-              </LinkContainer>
-          </Card.Body>
-        </Card>
-        )
-      })
+  const formatPathName = (str) => {
+    return str.replace(/\s/g , "-").toLowerCase();
+  }
+
+  const parksArray = props.parks?.map(park => {
+    return (
+      <Card style={{ width: '18rem' }} key={park.name} className={"m3"} >
+        <Card.Img variant="top" src={park.small_image} alt={park.name} />
+        <Card.Body>
+          <Card.Title>{park.name}</Card.Title>
+          <Card.Text>
+            Located in {park.location}
+          </Card.Text>
+            <LinkContainer to={`${match.url}/${formatPathName(park.name)}`}>
+              <Button
+                onClick={() => props.showPark(park)}
+                variant="primary"
+                className="rounded-pill"
+              >Discover
+              </Button>
+            </LinkContainer>
+        </Card.Body>
+      </Card>
+      )
+    })
       
     return (
       <div>
@@ -35,6 +40,5 @@ class ParksList extends Component {
       </div>
     )
   }
-};
 
 export default ParksList;
