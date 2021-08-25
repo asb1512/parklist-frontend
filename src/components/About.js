82b4fd22ee7c766288486c271d9a49f6f3be6c49
asb1512 <andrew@olympicmedia.com>
2021-08-25 14:59:50 -0400

    added heading for About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,11 @@
-function About(props) {
+import Container from 'react-bootstrap/Container';
 
+function About() {
+  return (
+    <Container className="m4">
+      <h1 className="poppins-heavy pl-navy">About</h1>
+    </Container>
+  )
 };
 
 export default About;
