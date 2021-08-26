fec8868564e08f4424f98ff1e8da4432eb6b7d73
asb1512 <andrew@olympicmedia.com>
2021-08-26 13:17:24 -0400

    converted to a class component

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,103 +1,30 @@
-import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { userSignup } from '../actions/parksActions';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
 
-export default function AuthForm() {
-  return (
-    <Tabs defaultActiveKey="login" className="mb-3">
-      <Tab eventKey="login" title="Login">
-        {LoginForm()}
-      </Tab>
-      <Tab eventKey="signup" title="Signup">
-        {SignupForm()}
-      </Tab>
-    </Tabs>
-  );
+class AuthForm extends Component {
+  render() {
+    return (
+      <Tabs defaultActiveKey="login" className="mb-3">
+        <Tab eventKey="login" title="Login">
+          <LoginForm />
+        </Tab>
+        <Tab eventKey="signup" title="Signup">
+          <SignupForm />
+        </Tab>
+      </Tabs>
+    );
+  };
 };
 
-function LoginForm() {
-
-  const [email, setEmail] = useState('')
-
-  const handleChange = (event) => {
-    setEmail(event.target.value)
+const mapDispatchToProps = dispatch => {
+  return {
+    userSignup: (user) => dispatch(userSignup(user))
   }
+}
 
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <FloatingLabel label='Enter email'>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            onChange={event => handleChange(event)}
-            value={email}
-          />
-        </FloatingLabel>
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Container className="d-grid">
-        <Button className="pl-green-bg rounded-pill no-border">
-          Login
-        </Button>
-      </Container>
-    </Form>
-  );
-};
-
-function SignupForm() {
-
-  const [email, setEmail] = useState('')
-  const [bio, setBio] = useState('')
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  }
-
-  const handleBioChange = event => {
-    setBio(event.target.value);
-  }
-
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <FloatingLabel label='Enter email'>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            onChange={event => handleEmailChange(event)}
-            value={email}
-          />
-        </FloatingLabel>
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3">
-        <FloatingLabel label="Enter your bio">
-          <Form.Control
-            as="textarea"
-            placeholder="Tell us a bit about yourself"
-            style={{ height: '100px' }}
-            onChange={event => handleBioChange(event)}
-            value={bio}
-          />
-        </FloatingLabel>
-      </Form.Group>
-
-      <Container className="d-grid">
-        <Button className="pl-green-bg rounded-pill no-border">
-          Sign up
-        </Button>
-      </Container>
-    </Form>
-  );
-};
+export default connect(null, userSignup())(AuthForm);
