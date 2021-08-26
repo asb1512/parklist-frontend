9be76ac0dd79b76f46bb1c0085ef44612973eec5
asb1512 <andrew@olympicmedia.com>
2021-08-26 12:02:35 -0400

    implemented React controlled forms for user login/signup

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,29 +1,42 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { FloatingLabel } from 'react-bootstrap';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 export default function AuthForm() {
   return (
     <Tabs defaultActiveKey="login" className="mb-3">
       <Tab eventKey="login" title="Login">
-        {loginForm()}
+        {LoginForm()}
       </Tab>
       <Tab eventKey="signup" title="Signup">
-        {signupForm()}
+        {SignupForm()}
       </Tab>
     </Tabs>
   );
 };
 
-function loginForm() {
+function LoginForm() {
+
+  const [email, setEmail] = useState('')
+
+  const handleChange = (event) => {
+    setEmail(event.target.value)
+  }
+
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <FloatingLabel label='Enter email'>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={event => handleChange(event)}
+            value={email}
+          />
         </FloatingLabel>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
@@ -39,16 +52,45 @@ function loginForm() {
   );
 };
 
-function signupForm() {
+function SignupForm() {
+
+  const [email, setEmail] = useState('')
+  const [bio, setBio] = useState('')
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleBioChange = event => {
+    setBio(event.target.value);
+  }
+
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <FloatingLabel label='Enter email'>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={event => handleEmailChange(event)}
+            value={email}
+          />
         </FloatingLabel>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <FloatingLabel label="Enter your bio">
+          <Form.Control
+            as="textarea"
+            placeholder="Tell us a bit about yourself"
+            style={{ height: '100px' }}
+            onChange={event => handleBioChange(event)}
+            value={bio}
+          />
+        </FloatingLabel>
       </Form.Group>
 
       <Container className="d-grid">
