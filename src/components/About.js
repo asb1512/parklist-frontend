618289e1b6ac8cc6775cc980628612ec31b92473
asb1512 <andrew@olympicmedia.com>
2021-08-26 10:06:30 -0400

    changed formatting

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,13 @@ function About() {
   return (
     <Container md={4}>
       <h1 className="poppins-bold pl-navy">About</h1>
-      <p className="poopins-thin">
+      <p className="poopins-thin pl-navy">
         At ParkList, we have a love for nature and the beauty provided by our 
         national park system. We believe that everyone should visit as many of 
-        the parks our nation has to offer as they can. And we're here to help 
-        you keep track of it all!
+        the parks our nation has to offer as they can.
+      </p>
+      <p className="poppins-bold pl-green">
+        And we're here to help you keep track of it all!
       </p>
     </Container>
   )
