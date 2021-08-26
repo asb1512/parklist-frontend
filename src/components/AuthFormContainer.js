c7e0c60199e7ba49a8425b01e8b7581de90e7dc0
asb1512 <andrew@olympicmedia.com>
2021-08-26 18:51:58 -0400

    implemented conditional rendering to show user's list once they're logged in

--- src/components/AuthFormContainer.js
+++ src/components/AuthFormContainer.js
@@ -8,15 +8,37 @@ import SignupForm from './SignupForm';
 
 class AuthFormContainer extends Component {
   render() {
+
+    const userAuth = () => {
+      return (
+        <Tabs defaultActiveKey="login" className="mb-3">
+          <Tab eventKey="login" title="Login">
+            <LoginForm />
+          </Tab>
+          <Tab eventKey="signup" title="Signup">
+            <SignupForm userSignup={this.props.userSignup} />
+          </Tab>
+        </Tabs>
+      )
+    }
+
+    const userLists = () => {
+      return (
+        <Tabs defaultActiveKey="visited" className="mb-3">
+          <Tab eventKey="visited" title="Visited">
+            <LoginForm />
+          </Tab>
+          <Tab eventKey="want-to-visit" title="Want to Visit">
+            <SignupForm userSignup={this.props.userSignup} />
+          </Tab>
+        </Tabs>
+      )
+    }
+
     return (
-      <Tabs defaultActiveKey="login" className="mb-3">
-        <Tab eventKey="login" title="Login">
-          <LoginForm />
-        </Tab>
-        <Tab eventKey="signup" title="Signup">
-          <SignupForm userSignup={this.props.userSignup} />
-        </Tab>
-      </Tabs>
+      <>
+        {this.props.currentUser ? userLists() : userAuth()}
+      </>
     );
   };
 };
