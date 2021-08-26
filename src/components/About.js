31386c1905eb43e539e53296b2d06635a842d02f
asb1512 <andrew@olympicmedia.com>
2021-08-25 21:29:06 -0400

    added paragraph to About page

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,14 @@ import Container from 'react-bootstrap/Container';
 
 function About() {
   return (
-    <Container className="m4">
-      <h1 className="poppins-heavy pl-navy">About</h1>
+    <Container md={4}>
+      <h1 className="poppins-bold pl-navy">About</h1>
+      <p className="poopins-thin">
+        At ParkList, we have a love for nature and the beauty provided by our 
+        national park system. We believe that everyone should visit as many of 
+        the parks our nation has to offer as they can. And we're here to help 
+        you keep track of it all!
+      </p>
     </Container>
   )
 };
