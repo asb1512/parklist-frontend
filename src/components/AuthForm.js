151450d3e728b337d3bb806604c34319ec767a57
asb1512 <andrew@olympicmedia.com>
2021-08-25 22:51:07 -0400

    added two basic forms for login/signup

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,15 +1,53 @@
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function AuthForm() {
   return (
     <Tabs defaultActiveKey="profile" className="mb-3">
       <Tab eventKey="home" title="Login">
-        
+        {loginForm()}
       </Tab>
       <Tab eventKey="profile" title="Signup">
-        
+        {signupForm()}
       </Tab>
     </Tabs>
-  )
+  );
+};
+
+function loginForm() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+  );
+};
+
+function signupForm() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Sign up
+      </Button>
+    </Form>
+  );
 };
