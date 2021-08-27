82e954a7ac865fbc298ade255ccf7c44212a7386
asb1512 <andrew@olympicmedia.com>
2021-08-26 22:11:43 -0400

    added return statement to prevent errors

--- src/components/presentational/VisitedList.js
+++ src/components/presentational/VisitedList.js
@@ -1,3 +1,5 @@
 export default function VisitedList() {
-  
+  return (
+    <div>Visited List</div>
+  )
 }
--- src/components/presentational/WantToVisitList.js
+++ src/components/presentational/WantToVisitList.js
@@ -1,3 +1,5 @@
 export default function WantToVisitList() {
-  
+  return (
+    <div>Want-to-Visit List</div>
+  )
 }
