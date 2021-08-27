71c173223c02e79bc282ab0c8a81eb8e9238bedd
asb1512 <andrew@olympicmedia.com>
2021-08-26 23:09:45 -0400

    renders a list of parks the user wants to visit

--- src/components/presentational/WantToVisitList.js
+++ src/components/presentational/WantToVisitList.js
@@ -1,5 +1,20 @@
-export default function WantToVisitList() {
+import ListGroup from 'react-bootstrap/ListGroup';
+
+export default function WantToVisitList(props) {
+
+  const desiredParksList = props.desiredParks?.map(desiredPark => {
+    let result = props.allParks.filter(park => desiredPark.park_id === park.id);
+    console.log("matching park", result[0])
+    return (
+      <ListGroup.Item>{result[0].name} National Park</ListGroup.Item>
+    )
+  })
+
   return (
-    <div>Want-to-Visit List</div>
+    <div>
+      <ListGroup>
+        {desiredParksList}
+      </ListGroup>
+    </div>
   )
 }
