84eda4b99ff74e07d3d0063a23e8759db57e299e
asb1512 <andrew@olympicmedia.com>
2021-08-27 17:12:07 -0400

    connected AdminContainer to Redux store

--- src/components/containers/AdminContainer.js
+++ src/components/containers/AdminContainer.js
@@ -1,18 +1,33 @@
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../../actions/parksActions';
 import UserList from "../presentational/UserList";
 
-function AdminContainer() {
-  return (
-    <>
-      <UserList />
-    </>
-  )
+class AdminContainer extends Component {
+
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  render() {
+    return (
+      <>
+        <UserList users={this.props.users} />
+      </>
+    )
+  }
 }
 
 const mapStateToProps = state => {
   return {
-    
+    users: state.users
   }
 }
 
-export default connect()(AdminContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminContainer);
