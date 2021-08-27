1b736ad6e6ed90457a0ec5f7f82dd4c435ec951e
asb1512 <andrew@olympicmedia.com>
2021-08-26 23:32:29 -0400

    converted to a functional component

--- src/components/presentational/Park.js
+++ src/components/presentational/Park.js
@@ -1,41 +1,36 @@
-import React, {Component} from 'react';
 import Image from 'react-bootstrap/Image';
 import Modal from 'react-bootstrap/Modal';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 
 
-class Park extends Component {
+export default function Park(props) {
 
-  render() {
-    return (
-      <Modal
-        show={this.props.show}
-        onHide={this.props.closeModal}
-        fullscreen
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>{this.props.park.name} National Park</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Image src={this.props.park.image} fluid />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.props.closeModal}>
-            Close
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.closeModal}
+      fullscreen
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{props.park.name} National Park</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Image src={props.park.image} fluid />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.closeModal}>
+          Close
+        </Button>
+        <ButtonGroup>
+          <Button variant="success" disabled>
+            Visited
           </Button>
-          <ButtonGroup>
-            <Button variant="success">
-              Visited
-            </Button>
-            <Button variant="warning">
-              Want to
-            </Button>
-          </ButtonGroup>
-        </Modal.Footer>
-      </Modal>
-    )
-  }
+          <Button variant="warning" disabled>
+            Want to
+          </Button>
+        </ButtonGroup>
+      </Modal.Footer>
+    </Modal>
+  )
 }
-
-export default Park;
