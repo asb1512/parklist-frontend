322f475b35d68a5eb466fd32390083e4a57c4947
asb1512 <andrew@olympicmedia.com>
2021-08-27 17:12:20 -0400

    created table to display users and their attributes

--- src/components/presentational/UserList.js
+++ src/components/presentational/UserList.js
@@ -1,11 +1,34 @@
-import ListGroup from "react-bootstrap/ListGroup";
+import Table from 'react-bootstrap/Table';
 
-export default function UserList() {
+export default function UserList(props) {
+
+  const userList = props.users?.map(user => {
+    console.log("UserList Map", user)
+    return (
+      <tr key={user.id}>
+        <td>{user.id}</td>
+        <td>{user.email}</td>
+        <td>{user.bio}</td>
+        <td>{user.admin ? "true" : "false"}</td>
+      </tr>
+    )
+  })
+
   return (
     <>
-      <ListGroup>
-        
-      </ListGroup>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Bio</th>
+            <th>Admin</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.users ? userList : null}
+        </tbody>
+      </Table>
     </>
   )
 }
