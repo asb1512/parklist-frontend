e296dca0ffa2fcd26ed42110f21783cbe7e0d714
asb1512 <andrew@olympicmedia.com>
2021-08-31 01:48:29 -0400

    coverted to function component

--- src/components/presentational/HomeCarousel.js
+++ src/components/presentational/HomeCarousel.js
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container'
 import Carousel from 'react-bootstrap/Carousel'
 
-class HomeCarousel extends Component {
+export default function HomeCarousel(props) {
 
-  render() {
-    const parksArray = this.props.parks?.map(park => {
-      return (
-        <Carousel.Item interval={5000} key={park.name}>
-          <img
-            className="d-block w-100"
-            src={park.image}
-            alt={park.name}
-          />
-          <Carousel.Caption>
-            <h3>{park.name} National Park</h3>
-            <p>{park.location}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      )
-    })
-
-    return (
-      <Container fluid className={"m0"}>
-        <Carousel>
-          {parksArray}
-        </Carousel>
-      </Container>
-    )
+  const parentStyle = {
+    position: 'relative',
+    textAlign: 'center',
   }
-}
 
-export default HomeCarousel;
+  const childStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
+
+  const parksArray = this.props.parks?.map(park => {
+    return (
+      <Carousel.Item
+        interval={5000}
+        key={park.name}
+        style={parentStyle}
+      >
+        <img
+          className="d-block w-100"
+          src={park.image}
+          alt={park.name}
+        />
+        <Carousel.Caption style={childStyle}>
+          <h3>{park.name} National Park</h3>
+          <p>{park.location}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    )
+  })
+
+  return (
+    <Container fluid className={"m0"}>
+      <Carousel>
+        {parksArray}
+      </Carousel>
+    </Container>
+  )
+}
