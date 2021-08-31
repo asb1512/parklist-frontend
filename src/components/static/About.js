f85e704f87173d71b40d0c5579379629b6185b03
asb1512 <andrew@olympicmedia.com>
2021-08-31 01:06:13 -0400

    formatting

--- src/components/static/About.js
+++ src/components/static/About.js
@@ -1,18 +1,21 @@
 import Container from 'react-bootstrap/Container';
+import LetterCounter from './LetterCounter';
 
 function About() {
   return (
-    <Container md={4}>
-      <h1 className="poppins-bold pl-navy">About</h1>
-      <p className="poopins-thin pl-navy">
-        At ParkList, we have a love for nature and the beauty provided by our 
-        national park system. We believe that everyone should visit as many of 
-        the parks our nation has to offer as they can.
-      </p>
-      <p className="poppins-bold pl-green">
-        And we're here to help you keep track of it all!
-      </p>
-    </Container>
+    <>
+      <Container md={4}>
+        <h1 className="poppins-bold pl-navy">About</h1>
+        <p className="poopins-thin pl-navy">
+          At ParkList, we have a love for nature and the beauty provided by our 
+          national park system. We believe that everyone should visit as many of 
+          the parks our nation has to offer as they can.
+        </p>
+        <p className="poppins-bold pl-green">
+          And we're here to help you keep track of it all!
+        </p>
+      </Container>
+    </>
   )
 };
 
